1aa630c73cba690c5c04b5cb101face8bc2e7814
Jethro <jethro@JethrodeMacBook-Pro.local>
2017-11-10 11:45:52 +0800

    finish user manage and homepage

--- SA/urls.py
+++ SA/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$',index, name='index'),
-    url(r'^login/$',login,name='login')
+    url(r'^user_login$', user_login, name='user_login'),
+    url(r'^user_register$', user_register, name='user_register'),
+    url(r'^home/$',home,name='home'),
 ]
--- SA/views.py
+++ SA/views.py
@@ -2,45 +2,69 @@ from django.shortcuts import render
 from .models import UserInfo
 from django.contrib.auth.models import User
 import json
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, render_to_response, \
+    HttpResponse, HttpResponseRedirect
 
+@csrf_exempt
 def index(request):
-    return render(request, 'SA/index.html', {})
+    return render(request, 'SA/index.html')
 
 @csrf_exempt
-def register(request):
-    if request.method == 'POST':
+def user_login(request):
+    if request.method == "POST":
         info = request.POST
         username = info['username']
         password = info['password']
-        email = info['email']
-
         try:
-            User.objects.create_user(username,email,password).save()
-        except:
-            return HttpResponse(json.dumps({'status':'error'}),content_type='application/json')
-        else:
-            return HttpResponse(json.dumps({'status':'success'}),content_type='application/json')
-    else:
-        return HttpResponse(json.dumps({'status':'error'}),content_type='application/json')
-
-
-@csrf_exempt
-def login(request):
-    if request.method == 'POST':
-        info = request.POST
-        username = info['username']
-        password = info['password']
+            User.objects.get(username=username)
+        except Exception:
+            result = {'status': 'error', 'error_message': 'user_not_exist'}
+            return HttpResponse(json.dumps(result), content_type='application/json')
         user = authenticate(request=request, username=username, password=password)
         if user and user.is_active:
-            login(request,user)
-            return HttpResponse(json.dumps({'status':'success'}),content_type='application/json')
-        else:
-            return HttpResponse(json.dumps({'status':'error'}),content_type='application/json')
+            login(request, user)
+            try:
+                info['remember_me']
+            except Exception:
+                request.session.set_expiry(0)
+            result = {'status': 'success'}
+            return HttpResponse(json.dumps(result), content_type='application/json')
+        result = {'status': 'error', 'error_message': 'wrong_password'}
+
+        return HttpResponse(json.dumps(result), content_type='application/json')
     else:
-        return HttpResponse(json.dumps({'status':'error'}),content_type='application/json')
+        return render_to_response('404.html')
 
 
+@csrf_exempt
+def user_register(request):
+    if request.method == "POST":
+        info = request.POST
+        username = info['username']
+        try:
+            User.objects.get(username=username)
+        except Exception:
+            password = info['password']
+            email = info['email']
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+            except Exception:
+                result = {'status': 'error', 'error_message': 'other'}
+                return HttpResponse(json.dumps(result), content_type='application/json')
+            else:
+                UserInfo(user=user).save()
+                result = {'status': 'success'}
+                return HttpResponse(json.dumps(result), content_type='application/json')
+        else:
+            result = {'status': 'error', 'error_message': 'user_exist'}
+            return HttpResponse(json.dumps(result), content_type='application/json')
+    else:
+        return render_to_response('404.html')
+
+@csrf_exempt
+def home(request):
+    return render(request,'SA/home.html',{})

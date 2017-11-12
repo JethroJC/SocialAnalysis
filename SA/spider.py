c3faa41e4ece89e59f555ccf1b22d3b784f0db1f
Jethro <jethro@JethrodeMacBook-Pro.local>
2017-11-12 09:38:24 +0800

    finish some work in person page and define some interface about spider

---
+++ SA/spider.py
@@ -0,0 +1,22 @@
+def get_weibo_profile(username,homepage_url):
+    '''
+    功能 : 根据用户名 和 用户主页地址 返回用户基本信息
+
+    flag : 0 表示成功获取到用户基本信息
+           1 表示该用户不存在 或 url不存在
+    img : 用户头像
+    lcoation : 用户位置
+    profile : 用户简介
+    '''
+    flag = img = location = profile = ''
+    return flag,img,location,profile
+
+def get_weibo_state(username):
+    '''
+    功能 ： 根据用户名 返回 爬取到的该用户的动态（动态内容、发布时间等）
+
+
+    :param username:
+    :return:
+    '''
+    pass

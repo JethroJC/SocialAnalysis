c9a2fe34008d1f47ca69862f5b63dd94d7bfe53c
AlanXue1997 <836106158@qq.com>
2017-11-13 14:12:48 +0800

    初次完成微博爬虫接口

--- SA/spider.py
+++ SA/spider.py
@@ -1,4 +1,7 @@
-def get_weibo_profile(username,homepage_url):
+import pymongo
+from scrapy import Item, Field
+
+def get_weibo_profile(weibo_id):
     '''
     功能 : 根据用户名 和 用户主页地址 返回用户基本信息
 
@@ -8,15 +11,36 @@ def get_weibo_profile(username,homepage_url):
     lcoation : 用户位置
     profile : 用户简介
     '''
-    flag = img = location = profile = ''
-    return flag,img,location,profile
 
-def get_weibo_state(username):
+    conn = pymongo.MongoClient("localhost", 27017)
+    db = conn["Sina"]
+    Information = db['Information']
+    item = Information.find_one({'_id': weibo_id})
+    if item:
+        return dict(item)
+    else:
+        return {}
+
+def get_weibo_state(weibo_id):
     '''
     功能 ： 根据用户名 返回 爬取到的该用户的动态（动态内容、发布时间等）
-
 
     :param username:
     :return:
     '''
-    pass
+
+    conn = pymongo.MongoClient("localhost", 27017)
+    db = conn["Sina"]
+    Tweets = db['Tweets']
+    item = Tweets.find({'ID': weibo_id})
+    if item:
+        return  [dict(x) for x in item]#dict(item[0])
+    else:
+        return []
+
+if __name__ == "__main__":
+    try:
+        print(get_weibo_profile('5066999620'))
+        print(get_weibo_state('5066999620'))
+    except Exception as e:
+        print(e)
